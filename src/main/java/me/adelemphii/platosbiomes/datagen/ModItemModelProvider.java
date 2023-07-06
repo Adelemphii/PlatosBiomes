2a592342146a91e5e30797258d266f985c287709
Adelemphii <demilitioowner@gmail.com>
2023-07-06 11:23:33 -0500

    trapdoors item model

--- src/main/java/me/adelemphii/platosbiomes/datagen/ModItemModelProvider.java
+++ src/main/java/me/adelemphii/platosbiomes/datagen/ModItemModelProvider.java
@@ -6,6 +6,7 @@ import net.minecraft.data.PackOutput;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.TrapDoorBlock;
 import net.minecraftforge.client.model.generators.ItemModelBuilder;
 import net.minecraftforge.client.model.generators.ItemModelProvider;
 import net.minecraftforge.common.data.ExistingFileHelper;
@@ -20,14 +21,17 @@ public class ModItemModelProvider extends ItemModelProvider {
     @Override
     protected void registerModels() {
         saplingItem(ModBlocks.EBONY_SAPLING);
-        this.withExistingParent("aracara_trapdoor", "handheld")
-                .texture("layer0", new ResourceLocation(PlatosBiomes.MODID, "item/aracara_trapdoor"));
+        trapdoorItem(ModBlocks.ARACARA_TRAPDOOR, "aracara_trapdoor");
     }
 
     private ItemModelBuilder saplingItem(RegistryObject<Block> item) {
         return withExistingParent(item.getId().getPath(),
                 new ResourceLocation("item/generated"))
                 .texture("layer0", new ResourceLocation(PlatosBiomes.MODID, "block/" + item.getId().getPath()));
+    }
+
+    private ItemModelBuilder trapdoorItem(RegistryObject<TrapDoorBlock> item, String name) {
+        return this.withExistingParent(name, modLoc("block/" + name + "_bottom"));
     }
 
     private ItemModelBuilder simpleItem(RegistryObject<Item> item) {
