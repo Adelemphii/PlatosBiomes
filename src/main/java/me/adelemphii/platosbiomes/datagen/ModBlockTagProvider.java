2c39f39e901f979cfb05e863e0a44c46750f65fb
Adelemphii <demilitioowner@gmail.com>
2023-07-07 09:54:05 -0500

    add blocks to block tags

--- src/main/java/me/adelemphii/platosbiomes/datagen/ModBlockTagProvider.java
+++ src/main/java/me/adelemphii/platosbiomes/datagen/ModBlockTagProvider.java
@@ -8,6 +8,7 @@ import net.minecraft.data.PackOutput;
 import net.minecraft.tags.BlockTags;
 import net.minecraftforge.common.data.BlockTagsProvider;
 import net.minecraftforge.common.data.ExistingFileHelper;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.concurrent.CompletableFuture;
@@ -20,7 +21,7 @@ public class ModBlockTagProvider extends BlockTagsProvider {
     }
 
     @Override
-    protected void addTags(HolderLookup.Provider provider) {
+    protected void addTags(HolderLookup.@NotNull Provider provider) {
         this.tag(ModTags.Blocks.ARACARA_LOGS)
                 .add(ModBlocks.ARACARA_LOG.get())
                 .add(ModBlocks.STRIPPED_ARACARA_LOG.get());
@@ -32,19 +33,50 @@ public class ModBlockTagProvider extends BlockTagsProvider {
     }
 
     private void addToMinecraftTags() {
-        this.tag(BlockTags.FENCES)
-                .add(ModBlocks.ARACARA_FENCE.get());
         this.tag(BlockTags.LOGS)
                 .add(ModBlocks.ARACARA_LOG.get())
                 .add(ModBlocks.STRIPPED_ARACARA_LOG.get())
                 .add(ModBlocks.ARACARA_WOOD.get())
                 .add(ModBlocks.STRIPPED_ARACARA_WOOD.get());
+        this.tag(BlockTags.PLANKS)
+                .add(ModBlocks.ARACARA_PLANKS.get());
+        this.tag(BlockTags.FENCES)
+                .add(ModBlocks.ARACARA_FENCE.get());
         this.tag(BlockTags.SIGNS)
                 .add(ModBlocks.ARACARA_SIGN.get())
                 .add(ModBlocks.ARACARA_WALL_SIGN.get());
+        this.tag(BlockTags.STANDING_SIGNS)
+                .add(ModBlocks.ARACARA_SIGN.get());
         this.tag(BlockTags.WALL_SIGNS)
                 .add(ModBlocks.ARACARA_WALL_SIGN.get());
         this.tag(BlockTags.STANDING_SIGNS)
                 .add(ModBlocks.ARACARA_SIGN.get());
+        this.tag(BlockTags.SLABS)
+                .add(ModBlocks.ARACARA_SLAB.get());
+        this.tag(BlockTags.FENCES)
+                .add(ModBlocks.ARACARA_FENCE.get());
+        this.tag(BlockTags.FENCE_GATES);
+        this.tag(BlockTags.PRESSURE_PLATES)
+                .add(ModBlocks.ARACARA_PRESSURE_PLATE.get());
+        this.tag(BlockTags.WOODEN_PRESSURE_PLATES)
+                .add(ModBlocks.ARACARA_PRESSURE_PLATE.get());
+        this.tag(BlockTags.STAIRS)
+                .add(ModBlocks.ARACARA_STAIRS.get());
+        this.tag(BlockTags.WOODEN_STAIRS)
+                .add(ModBlocks.ARACARA_STAIRS.get());
+        this.tag(BlockTags.BUTTONS)
+                .add(ModBlocks.ARACARA_BUTTON.get());
+        this.tag(BlockTags.WOODEN_BUTTONS)
+                .add(ModBlocks.ARACARA_BUTTON.get());
+        this.tag(BlockTags.LEAVES)
+                .add(ModBlocks.ARACARA_LEAVES.get());
+        this.tag(BlockTags.DOORS)
+                .add(ModBlocks.ARACARA_DOOR.get());
+        this.tag(BlockTags.WOODEN_DOORS)
+                .add(ModBlocks.ARACARA_DOOR.get());
+        this.tag(BlockTags.TRAPDOORS)
+                .add(ModBlocks.ARACARA_TRAPDOOR.get());
+        this.tag(BlockTags.WOODEN_TRAPDOORS)
+                .add(ModBlocks.ARACARA_TRAPDOOR.get());
     }
 }
